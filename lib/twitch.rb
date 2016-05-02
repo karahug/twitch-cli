1c7041e415476a5bd21292a2ba6638930bdd54bf
Tyrone DeSilva <tdesilva@asu.edu>
2016-05-02 23:25:32 +0000

    suppressed output, decommented ssl block

--- lib/twitch.rb
+++ lib/twitch.rb
@@ -1,10 +1,10 @@
 require_relative "twitch_classes"
-=begin
+
 # necessary for windows
 require "openssl"
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-=end
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE; nil
+
 
 puts "Welcome to twitch-cmd app"
 
